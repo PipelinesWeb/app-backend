91a92e5433c621a2f07078c2f98392a536d62d95
ccmacas <celiamacas24@gmail.com>
2023-06-25 13:51:41 -0500

    fix: Habilitar CORS

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.set("port", process.env.PORT || 3000);
 
 // middlewares
 app.use(morgan("dev"));
+app.use(cors()); // Habilitar CORS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
